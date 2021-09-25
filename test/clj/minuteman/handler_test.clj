53f598dd757c4249aec7e93fa7560046ffcb71bb
mangosmoothie <mangosmoothie@users.noreply.github.com>
2021-09-25 15:26:00 -0500

    got tests working again

--- test/clj/minuteman/handler_test.clj
+++ test/clj/minuteman/handler_test.clj
@@ -3,28 +3,33 @@
    [clojure.test :refer [is deftest testing use-fixtures]]
    [luminus-migrations.core :as migrations]
    [minuteman.config :refer [env]]
-   [minuteman.db.core :refer [*db*] :as db]
+   [minuteman.db.core :as db]
    [minuteman.handler :refer [app]]
-   [minuteman.middleware :refer [wrap-base wrap-formats]]
+   [minuteman.middleware]
    [minuteman.middleware.formats :as formats]
    [minuteman.routes.api :refer [api-routes]]
    [mount.core :as mount]
    [muuntaja.core :as m]
    [reitit.ring :as ring]
    [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
-   [ring.middleware.params :refer [wrap-params]]
-   [ring.mock.request :refer [request]]))
+   [ring.mock.request :refer [request]]
+   [ring.util.http-predicates :refer [ok? created? not-found? no-content?]]))
 
 (defn parse-json [body]
   (m/decode formats/instance "application/json" body))
 
-(def post-route-handler
+(def route-handler
   "need this to get around CSRF middleware"
-  (-> (ring/ring-handler (ring/router [(api-routes)]))
-      (wrap-defaults
-       (-> site-defaults
-           (dissoc :security)
-           (dissoc :session)))))
+  (let [f (fn [handler]
+            (fn ([r] (handler r))
+              ([req res raise]
+               (handler req res raise))))]
+    (with-redefs [minuteman.middleware/wrap-csrf f]
+      (-> (ring/ring-handler (ring/router [(api-routes)]))
+          (wrap-defaults
+           (-> site-defaults
+               (dissoc :security)
+               (dissoc :session)))))))
 
 (def test-es-instance {:name "test_name" :url "test_url"})
 
@@ -48,48 +53,48 @@
 (deftest test-app
   (testing "main route"
     (let [response ((app) (request :get "/"))]
-      (is (= 200 (:status response)))))
+      (is (ok? response))))
 
   (testing "not-found route"
     (let [response ((app) (request :get "/invalid"))]
-      (is (= 404 (:status response)))))
+      (is (not-found? response))))
 
   (testing "create es-instance route"
-    (let [response (post-route-handler
+    (let [response (route-handler
                     (request :post "/api/es-instances" test-es-instance))]
-      (is (= 201 (:status response)))))
+      (is (created? response))))
 
   (testing "get es-instances route"
     (let [response ((app) (request :get "/api/es-instances"))
           data (-> response :body parse-json :data)]
-      (is (= 200 (:status response)))
+      (is (ok? response))
       (is (seq data))
       (is (seq (filter #(= (:name test-es-instance) (:name %)) data)))))
 
   (testing "create es-index route"
     (let [es-instance-id (get-es-instance-id)
-          response (post-route-handler
+          response (route-handler
                     (request :post "/api/es-indices"
                              (assoc test-es-index :es_instance_id es-instance-id)))]
-      (is (= 201 (:status response)))))
+      (is (created? response))))
 
   (testing "get es-indices route"
     (let [response ((app) (request :get "/api/es-indices"))
           data (-> response :body parse-json :data)]
-      (is (= 200 (:status response)))
+      (is (ok? response))
       (is (seq data))))
 
   (testing "toggle es-index watch"
     (let [es-index-id (-> (db/get-es-indices) first :id)
           toggle-watch (fn [s]
-                         (post-route-handler
+                         (route-handler
                           (request :put (str "/api/es-indices/"
                                              es-index-id "/" s))))
           response-watch (toggle-watch "watch")
           es-index-watched (db/get-es-index {:id es-index-id})
           response-unwatch (toggle-watch "unwatch")
           es-index-unwatched (db/get-es-index {:id es-index-id})]
-      (is (= 204 (:status response-watch)))
+      (is (no-content? response-watch))
       (is (:watching es-index-watched))
-      (is (= 204 (:status response-unwatch)))
+      (is (no-content? response-unwatch))
       (is (not (:watching es-index-unwatched))))))
